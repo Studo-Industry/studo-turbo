6985202eee02717bd75e4b8b43ac660650308db5
sanket-16 <san162002@gmail.com>
2023-07-09 22:19:43 +0530

    chore: adding legal stuff

--- apps/web/src/components/Footer.tsx
+++ apps/web/src/components/Footer.tsx
@@ -1,62 +1,68 @@
-import React from "react";
-import { IoLogoWhatsapp } from "react-icons/io";
-import { RiInstagramFill } from "react-icons/ri";
-import { AiFillTwitterCircle } from "react-icons/ai";
+import Link from 'next/link';
+import { IoLogoWhatsapp } from 'react-icons/io';
+import { RiInstagramFill } from 'react-icons/ri';
+import { AiFillTwitterCircle } from 'react-icons/ai';
 
 const Footer = () => {
   return (
     <div
-      className="blue-orange-gradient scroll-smooth bg-gradient-to-bl"
-      id="footer"
+      className='blue-orange-gradient scroll-smooth bg-gradient-to-bl'
+      id='footer'
     >
-      <div className=" mb-0 flex flex-col px-10 py-6 pt-12 text-white md:flex-row md:justify-between md:px-32 ">
+      <div className=' mb-0 flex flex-col px-10 py-6 pt-12 text-white md:flex-row md:justify-between md:px-32 '>
         <div>
-          <div className="flex flex-col items-start gap-4 md:flex-row">
+          <div className='flex flex-col items-start gap-4 md:flex-row'>
             <div>
-              <h1 className="text-4xl font-bold ">StudioIndustry</h1>
-              <p className="pt-2 text-xs">
+              <h1 className='text-4xl font-bold '>StudioIndustry</h1>
+              <p className='pt-2 text-xs'>
                 Lorem ipsum dolor sit amet consectetur <br /> adipisicing elit.
                 Porro, fuga.
               </p>
             </div>
-            <button className="Button blue-orange-gradient rounded-full bg-gradient-to-bl px-1 py-1">
-              <div className="h-full w-full whitespace-nowrap rounded-full bg-white px-8 py-2 text-base font-semibold text-gray-600">
+            <button className='Button blue-orange-gradient rounded-full bg-gradient-to-bl px-1 py-1'>
+              <div className='h-full w-full whitespace-nowrap rounded-full bg-white px-8 py-2 text-base font-semibold text-gray-600'>
                 Download App
               </div>
             </button>
           </div>
-          <ul className="mt-10 flex flex-col gap-2 text-sm">
-            <li className="text-base font-bold">Quick Links</li>
-            <li>About Us</li>
-            <li>Terms and Conditions</li>
-            <li>Privacy Policy</li>
+          <ul className='mt-10 flex flex-col gap-2 text-sm'>
+            <li className='text-base font-bold'>Quick Links</li>
+            <li>
+              <Link href='/about'>About Us</Link>
+            </li>
+            <li>
+              <Link href='/legal/terms'>Terms and Conditions</Link>
+            </li>
+            <li>
+              <Link href='/legal/privacy-policy'>Privacy Policy</Link>
+            </li>
           </ul>
         </div>
 
-        <div className="my-12 md:my-0">
-          <div className="flex flex-col gap-3 md:flex-row">
-            <h1 className="mt-2 text-2xl font-medium">Keep In Touch.</h1>
+        <div className='my-12 md:my-0'>
+          <div className='flex flex-col gap-3 md:flex-row'>
+            <h1 className='mt-2 text-2xl font-medium'>Keep In Touch.</h1>
 
-            <ul className="mx-3 mt-2 flex gap-6 text-3xl md:justify-evenly">
+            <ul className='mx-3 mt-2 flex gap-6 text-3xl md:justify-evenly'>
               <a
-                href="https://www.instagram.com/studoindustry_projects/"
-                target="new"
+                href='https://www.instagram.com/studoindustry_projects/'
+                target='new'
               >
                 <RiInstagramFill />
               </a>
-              <a href="#">
+              <a href='#'>
                 <AiFillTwitterCircle />
               </a>
-              <a href="#">
+              <a href='#'>
                 <IoLogoWhatsapp />
               </a>
             </ul>
           </div>
 
-          <h3 className="mt-10 text-base font-bold">Contact Us</h3>
-          <ul className="mt-4 text-sm ">
+          <h3 className='mt-10 text-base font-bold'>Contact Us</h3>
+          <ul className='mt-4 text-sm '>
             <li>
-              Address - Lorem ipsum dolor sit amet consectetur <br />{" "}
+              Address - Lorem ipsum dolor sit amet consectetur <br />{' '}
               adipisicing elit. Repudiandae cumque voluptatem facilis!
             </li>
             <li>Email - studioindustry@gmail.com</li>
@@ -65,7 +71,7 @@ const Footer = () => {
         </div>
       </div>
 
-      <p className="pb-2 text-center text-xs text-white">
+      <p className='pb-2 text-center text-xs text-white'>
         &#169; Studo Industry 2023
       </p>
     </div>
---
+++ apps/web/src/pages/legal/terms.tsx
@@ -0,0 +1,149 @@
+const Terms = () => {
+  return (
+    <div className='mx-10 my-6 flex flex-col gap-6 whitespace-pre-wrap md:mx-20'>
+      <h3 className='text-xl font-bold'>Terms and Conditions - </h3>
+      <p>
+        Please read these Terms and Conditions ("Terms") carefully before using
+        the Studoindustry platform ("Platform"). These Terms constitute a
+        legally binding agreement between you ("User" or "you") and
+        Studoindustry ("we," "us," or "our"). By accessing or using the
+        Platform, you agree to be bound by these Terms.
+      </p>
+      <div>
+        <h5 className='font-bold'>1. Platform Usage</h5>
+        <p>
+          a. The Platform provides a medium for students to access and
+          participate in industrial projects facilitated by Studoindustry. b.
+          Users may browse, search, and apply for available projects through the
+          Platform.
+        </p>
+      </div>
+      <div>
+        <h5 className='font-bold'>2. Project Engagement</h5>
+        <p>
+          a. Users acknowledge that Studoindustry acts as a mediator between
+          students and industry partners for project facilitation. b. Projects
+          made available through the Platform are subject to the terms and
+          conditions set by the respective industry partners. c. Users agree to
+          comply with the guidelines, requirements, and instructions provided by
+          Studoindustry and the industry partners.
+        </p>
+      </div>
+      <div>
+        <h5 className='font-bold'>3. Intellectual Property</h5>
+        <p>
+          a. Any work, ideas, inventions, or contributions created during the
+          project will be subject to the intellectual property rights of the
+          respective industry partner. b. Users shall not claim ownership or
+          seek any intellectual property rights over the project or its
+          outcomes. c. Students shall not patent the industrial projects
+          facilitated by Studoindustry.
+        </p>
+      </div>
+      <div>
+        <h5 className='font-bold'>4. Confidentiality and Non-Disclosure</h5>
+        <p>
+          a. Users shall maintain strict confidentiality regarding any sensitive
+          or proprietary information shared during the project. b. Students
+          shall not disclose the project details or outcomes to any other
+          company without the explicit consent of the industry partner. c.
+          Students shall not give the project to any other company or third
+          party.
+        </p>
+      </div>
+      <div>
+        <h5 className='font-bold'>5. Funding and Recognition</h5>
+        <p>
+          a. Students who provide the optimum solution as determined by the
+          industry partner will be eligible to receive funding from the industry
+          partner. b. Other participants who do not receive funding will be
+          awarded a certificate of participation. c. The decision to provide
+          funding or certificates solely rests with Studoindustry and the
+          respective industry partner.
+        </p>
+      </div>
+      <div>
+        <h5 className='font-bold'>6. Liability and Responsibility</h5>
+        <p>
+          a. Studoindustry shall not be held liable for any direct, indirect,
+          incidental, consequential, or special damages arising out of or in
+          connection with the use of the Platform or the projects facilitated
+          through it. b. All terms and conditions, including the allocation of
+          funds, recognition, and certificates, are reserved by Studoindustry.
+        </p>
+      </div>
+      <div>
+        <h5 className='font-bold'>7. Participants' Certificates</h5>
+        <p>
+          a. All participating students will receive a certificate from the
+          respective industry partner upon successful completion of the project.
+        </p>
+      </div>
+      <div>
+        <h5 className='font-bold'>8. Termination</h5>
+        <p>
+          a. Studoindustry reserves the right to suspend or terminate a user's
+          access to the Platform at any time, without prior notice or liability,
+          for any violation of these Terms or any other valid reason.
+        </p>
+      </div>
+      <div>
+        <h5 className='font-bold'>9. Governing Law</h5>
+        <p>
+          a. These Terms shall be governed by and construed in accordance with
+          the laws of [Jurisdiction]. Any legal action or proceeding arising out
+          of or relating to these Terms shall be exclusively brought in the
+          courts located in [Jurisdiction].
+        </p>
+      </div>
+      <div>
+        <h5 className='font-bold'>10. Severability</h5>
+        <p>
+          a. If any provision of these Terms is found to be invalid or
+          unenforceable, the remaining provisions shall remain in full force and
+          effect.
+        </p>
+      </div>
+      <div>
+        <h5 className='font-bold'>11. Modifications</h5>
+        <p>
+          a. Studoindustry reserves the right to modify or update these Terms at
+          any time. Users will be notified of any changes, and continued use of
+          the Platform after the modifications constitute acceptance of the
+          updated Terms.
+        </p>
+      </div>
+      <div>
+        <h5 className='font-bold'>12. Eligibility</h5>
+        <p>
+          a. Users must be at least 18 years old to access and use the Platform.
+          b. Users must provide accurate and up-to-date information during the
+          registration process.
+        </p>
+      </div>
+      <div>
+        <h5 className='font-bold'>13. Code of Conduct</h5>
+        <p>
+          a. Users shall conduct themselves in a professional and respectful
+          manner throughout their engagement with the Platform and the project.
+          b. Users shall not engage in any behavior that may harm the reputation
+          or integrity of Studoindustry or the industry partner.
+        </p>
+      </div>
+      <div>
+        <h5 className='font-bold'>14. Entire Agreement</h5>
+        <p>
+          a. These Terms constitute the entire agreement between the user and
+          Studoindustry regarding the use of the Platform and supersede any
+          prior agreements or understandings.
+        </p>
+      </div>
+      <p>
+        By accessing or using the Studoindustry platform, you acknowledge that
+        you have read, understood, and agree to be bound by these Terms.
+      </p>
+    </div>
+  );
+};
+
+export default Terms;
