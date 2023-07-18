5b65cd06a096701481b5f5a35b3ac858e554be79
sidmadke <siddhant.madke@gmail.com>
2023-07-18 23:16:43 +0530

    feat: FAQs added

--- apps/web/src/pages/help/index.tsx
+++ apps/web/src/pages/help/index.tsx
@@ -1,5 +1,21 @@
-const Help = () => {
-  return <div className='mx-10 my-10 md:mx-20'>Help Page</div>;
-};
+import React from 'react'
+import FAQ from '~/components/FAQ'
 
-export default Help;
+
+const questionNo = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18]
+const Help = () => {
+  return (
+    <div>
+      <div>
+        <h1 className='my-10 mx-10 text-2xl font-bold'>FAQ's</h1>
+        <div>
+          {questionNo?.map((question)=>
+            <FAQ que={question}/>
+          )}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Help
