e80191a1f2fab1b8d1ce309392a11274e2937b86
sanket-16 <san162002@gmail.com>
2023-07-20 22:52:56 +0530

    fix: changes

--- apps/web/src/pages/admin/dashboard/approval/index.tsx
+++ apps/web/src/pages/admin/dashboard/approval/index.tsx
@@ -1,27 +1,28 @@
-import React from 'react'
+import React from 'react';
 import { ApprovalTeamCard } from '~/components/Cards';
 import Error from '~/components/Error';
 import PreLoader from '~/components/PreLoader';
-import { api } from '~/utils/api'
+import { api } from '~/utils/api';
 
 const approval = () => {
-    const {data:teamsData,status:teamsStatus}=api.team.getApprovalRequestingTeams.useQuery();
-if(teamsStatus==="loading"){
-  return <PreLoader/>
-}
-if(teamsStatus==="error"){
-  return <Error error='Errorrrrr'/>
-}
+  const { data: teamsData, status: teamsStatus } =
+    api.team.getApprovalRequestingTeams.useQuery();
+  if (teamsStatus === 'loading') {
+    return <PreLoader />;
+  }
+  if (teamsStatus === 'error') {
+    return <Error error='Errorrrrr' />;
+  }
   return (
     <div className='m-10'>
-        <h1 className='text-center text-3xl font-bold mb-10'>Payment Approval</h1>
-        <div className='grid grid-cols-2 gap-6'>
-            {teamsData?.map((team)=>{
-                return <ApprovalTeamCard team={team} />
-            })}
-        </div>
+      <h1 className='mb-10 text-center text-3xl font-bold'>Payment Approval</h1>
+      <div className='grid grid-cols-2 gap-6'>
+        {teamsData?.map((team) => {
+          return <ApprovalTeamCard team={team} key={team.id} />;
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default approval
+export default approval;
