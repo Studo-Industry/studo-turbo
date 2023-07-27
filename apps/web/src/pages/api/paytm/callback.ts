9ce55f70a896e584f424496e6d84b2e04346448c
sanket-16 <san162002@gmail.com>
2023-07-27 18:51:24 +0530

    fix: paytm cb #2

--- apps/web/src/pages/api/paytm/callback.ts
+++ apps/web/src/pages/api/paytm/callback.ts
@@ -1,12 +1,8 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  if (req.method === 'POST') {
-    console.log(req);
-    res.redirect(
-      `/dashboard/team/thankyou?teamId=${req.query.teamId}&orderId=${req.query.orderId}`,
-    );
-  } else {
-    // Handle any other HTTP method
-  }
+  console.log(req);
+  res.redirect(
+    `/dashboard/team/thankyou?teamId=${req.query.teamId}&orderId=${req.query.orderId}`,
+  );
 }
