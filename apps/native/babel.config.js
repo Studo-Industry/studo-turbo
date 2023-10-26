7f4370ebc60c14775ce9b05ba5b8b8a4959ba2ac
ABHISHEK THORAT <abhithorat.24@gmail.com>
2023-10-27 02:08:17 +0530

    mobile application

--- apps/native/babel.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import("@babel/core").ConfigFunction} */
-module.exports = function (api) {
-  // process.env.EXPO_ROUTER_APP_ROOT = '../../apps/expo/app';
-
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
-};
--- apps/web/src/pages/api/auth/[...nextauth].ts
+++
@@ -1,4 +0,0 @@
-import NextAuth from 'next-auth';
-import { authOptions } from 'server';
-
-export default NextAuth(authOptions);
